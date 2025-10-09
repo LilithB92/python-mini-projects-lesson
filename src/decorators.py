462294ac690e39f5e93208b0e705bf1a6f76ccd2
Lilith <bichakhchyanlilith1992@gmail.com>
2025-10-09 13:02:46 +0300

    added decorator log and its tests

---
+++ src/decorators.py
@@ -0,0 +1,36 @@
+import os
+from datetime import datetime
+from functools import wraps
+from typing import Any
+from typing import Callable
+from typing import Union
+
+
+def log(filename: Union[str | None] = None) -> Callable[..., Any]:
+    def wrapper(func: Callable[..., Any]) -> Callable[..., Any]:
+        @wraps(func)
+        def inner(*args: Any, **kwargs: Any) -> Any:
+            current_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            func_message = ""
+            try:
+                result = func(*args, **kwargs)
+                func_message += f"{func.__name__} ok"
+                return result
+            except Exception as e:
+                exception_type = type(e).__name__
+                func_message += f"{func.__name__} error: {exception_type}. Inputs: {args}, {kwargs}"
+            finally:
+                if filename:
+                    directory_name = os.path.split(os.getcwd())[0]
+                    log_dir = os.path.join(directory_name, "data")
+                    if not os.path.exists(log_dir):
+                        os.makedirs(log_dir)
+                    log_file = os.path.join(log_dir, filename + ".txt")
+                    with open(log_file, "w") as file:
+                        file.write(f"{current_date} {func_message}\n")
+                else:
+                    print(func_message)
+
+        return inner
+
+    return wrapper
---
+++ tests/test_decorators.py
@@ -0,0 +1,53 @@
+import os
+from datetime import datetime
+from typing import Any
+
+import pytest
+
+from src.decorators import log
+
+
+def test_double_decorator(capsys: pytest.CaptureFixture[str]) -> None:
+    @log()
+    def my_function(x: int, y: int) -> int:
+        return x + y
+
+    my_function(3, 5)
+    captured = capsys.readouterr()
+    assert captured.out == "my_function ok\n"
+
+
+def test_invalid_datas_log(capsys: pytest.CaptureFixture[str]) -> None:
+    @log()
+    def value_error_raiser() -> Any:
+        raise ValueError("this is ValueError")
+
+        captured = value_error_raiser()
+        assert captured.err == "value_error_raiser error: ValueError. Inputs: (), {}"
+
+
+def test_write_logs_datas_in_file() -> None:
+    @log("hello")
+    def my_function(x: int, y: int) -> int:
+        return x + y
+
+    my_function(5, 4)
+    current_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    directory_name = os.path.split(os.getcwd())[0]
+    log_file = os.path.join(directory_name, "data", "hello.txt")
+    with open(log_file) as f:
+        assert f.read() == f"{current_date} my_function ok\n"
+
+
+def test_write_logs_invalid_datas_in_file() -> None:
+    @log("error_log")
+    def value_error_raiser() -> Any:
+        raise ValueError("this is error")
+
+    value_error_raiser()
+    current_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    with pytest.raises(AssertionError):
+        directory_name = os.path.split(os.getcwd())[0]
+        log_file = os.path.join(directory_name, "data", "error_log.txt")
+        with open(log_file) as f:
+            assert f.read() == current_date.join("value_error_raiser error: ValueError. Inputs: (), {}\n")
