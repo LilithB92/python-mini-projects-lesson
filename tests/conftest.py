da0dbabf1676206660d53bd168a2491683c1513c
Lilith <bichakhchyanlilith1992@gmail.com>
2025-10-20 00:30:17 +0300

    init test_external_api.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -74,3 +74,17 @@ def transactions_list() -> list:
             "to": "Счет 75651667383060284188",
         },
     ]
+@pytest.fixture()
+def transaction()->dict:
+    return  {
+    "id": 441945886,
+    "state": "EXECUTED",
+    "date": "2019-08-26T10:50:58.294041",
+    "operationAmount": {
+      "amount": "31957.58",
+      "currency": {
+        "name": "руб.",
+        "code": "RUB"
+      }
+    }
+    }
